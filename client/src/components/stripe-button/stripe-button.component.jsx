91d0480d9f18ec2a08f47b4354a82cd26da3e58c
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-07-01 01:30:08 +0530

    separating our cra app into client folder, added express server to help handle backend stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,29 +1,45 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
-    const priceForStripe = price * 100;
-    const publishableKey = 'pk_test_51Gt92lAHloLKFKhU77Od2GT8o7revPfpV6Yt6sW2XZjLHhYwjwFYRISG1boC6QuiaXJIe01RiS8kIFpMVijVQ8zy00fH3ZqkEB';
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51Gt92lAHloLKFKhU77Od2GT8o7revPfpV6Yt6sW2XZjLHhYwjwFYRISG1boC6QuiaXJIe01RiS8kIFpMVijVQ8zy00fH3ZqkEB';
 
-    const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
-    }
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
+  };
 
-    return (
-        <StripeCheckout 
-            label='Pay Now'
-            name='CRWN Clothing Ltd.'
-            billingAddress
-            shippingAddress
-            image='https://svgshare.com/i/CUz.svg'
-            description={`Your total is $${price}`}
-            amount={priceForStripe}
-            panelLabel='Pay Now'
-            token={onToken}
-            stripeKey={publishableKey}
-        />
-    )
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
 };
 
 export default StripeCheckoutButton;
